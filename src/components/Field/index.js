ccbbda4c4e1745b28a25e6009d30a4d0603e26e9
Pochat <52283864+SplinterNight1@users.noreply.github.com>
2024-02-20 17:48:09 +0100

    Update index.js

--- src/components/Field/index.js
+++ src/components/Field/index.js
@@ -1,3 +1,61 @@
+// import PropTypes from "prop-types";
+
+// import "./style.scss";
+
+// export const FIELD_TYPES = {
+//   INPUT_TEXT: 1,
+//   TEXTAREA: 2,
+// };
+
+// const Field = ({ type = FIELD_TYPES.INPUT_TEXT, label, name, placeholder }) => {
+//   let component;
+//   switch (type) {
+//     case FIELD_TYPES.INPUT_TEXT:
+//       component = (
+//         <input
+//           type="text"
+//           name={name}
+//           placeholder={placeholder}
+//           data-testid="field-testid"
+//         />
+//       );
+//       break;
+//     case FIELD_TYPES.TEXTAREA:
+//       component = <textarea name={name} data-testid="field-testid" />;
+//       break;
+//     default:
+//       component = (
+//         <input
+//           type="text"
+//           name={name}
+//           placeholder={placeholder}
+//           data-testid="field-testid"
+//         />
+//       );
+//   }
+//   return (
+//     <div className="inputField">
+//       <span>{label}</span>
+//       {component}
+//     </div>
+//   );
+// };
+
+// Field.propTypes = {
+//   type: PropTypes.oneOf(Object.values(FIELD_TYPES)),
+//   name: PropTypes.string,
+//   label: PropTypes.string,
+//   placeholder: PropTypes.string,
+// };
+//  Field.defaultProps = {
+//    label: "",
+//    placeholder: "",
+//    type: FIELD_TYPES.INPUT_TEXT,
+//    name: "field-name",
+//  }
+
+// export default Field;
+
 import PropTypes from "prop-types";
 
 import "./style.scss";
@@ -7,7 +65,7 @@ export const FIELD_TYPES = {
   TEXTAREA: 2,
 };
 
-const Field = ({ type = FIELD_TYPES.INPUT_TEXT, label, name, placeholder }) => {
+const Field = ({ type = FIELD_TYPES.INPUT_TEXT, label, name, placeholder, onChange }) => {
   let component;
   switch (type) {
     case FIELD_TYPES.INPUT_TEXT:
@@ -17,11 +75,23 @@ const Field = ({ type = FIELD_TYPES.INPUT_TEXT, label, name, placeholder }) => {
           name={name}
           placeholder={placeholder}
           data-testid="field-testid"
+          aria-label={name}
+          onChange={(e) => onChange(e.target.value)}
+          required
+          aria-required="true"
         />
       );
       break;
     case FIELD_TYPES.TEXTAREA:
-      component = <textarea name={name} data-testid="field-testid" />;
+      component = (
+        <textarea
+          name={name}
+          data-testid="field-testid"
+          onChange={(e) => onChange(e.target.value)}
+          required
+          aria-required="true"
+        />
+      );
       break;
     default:
       component = (
@@ -30,6 +100,9 @@ const Field = ({ type = FIELD_TYPES.INPUT_TEXT, label, name, placeholder }) => {
           name={name}
           placeholder={placeholder}
           data-testid="field-testid"
+          onChange={(e) => onChange(e.target.value)}
+          required
+          aria-required="true"
         />
       );
   }
@@ -46,12 +119,15 @@ Field.propTypes = {
   name: PropTypes.string,
   label: PropTypes.string,
   placeholder: PropTypes.string,
+  onChange: PropTypes.func, // Ajout de la prop onChange
 };
- Field.defaultProps = {
-   label: "",
-   placeholder: "",
-   type: FIELD_TYPES.INPUT_TEXT,
-   name: "field-name",
- }
+
+Field.defaultProps = {
+  label: "",
+  placeholder: "",
+  type: FIELD_TYPES.INPUT_TEXT,
+  name: "field-name",
+  onChange: () => null, // Définition d'une fonction vide par défaut
+};
 
 export default Field;
